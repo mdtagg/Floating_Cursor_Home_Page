b18b3f623890e166b8c135dea5b6b170a721a9a6
Mike Tagg <mdtagg91@gmail.com>
2023-09-05 14:54:21 -0700

    main element and carosel component within main added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import Carosel from './Carosel'
 
 function App() {
 
   return (
     <>
-      
+      <main>
+        <h2>FEATURED ENGAGEMENTS</h2>
+        <Carosel/>
+      </main>
     </>
   )
 }
