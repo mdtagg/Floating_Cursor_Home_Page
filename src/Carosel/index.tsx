3798a842d9c97edfd86cec6f1bba7afcb29bfa82
Mike Tagg <mdtagg91@gmail.com>
2023-09-06 13:42:14 -0700

    custom cursor now hovering over regular cursor, issues with position calculations in relation to viewport size

--- src/Carosel/index.tsx
+++ src/Carosel/index.tsx
@@ -18,31 +18,33 @@ const Carosel = () => {
     }
 
     function handleMouseEnter(e:React.MouseEvent<HTMLUListElement, MouseEvent>) {
-        // console.log(e)
         e.stopPropagation()
-        const { clientX,clientY } = e
-        console.log(clientX,clientY)
-        const { offsetLeft,offsetTop } = cursor.current!
-        console.log(offsetLeft,offsetTop)
-        const test = clientX - offsetLeft
-        const testTwo = clientY - offsetTop
-        console.log(test,testTwo)
-        cursor.current!.style.translate = `${test}px ${testTwo}px`
+        let offsetLeft = cursor.current!.offsetLeft
+        let offsetTop = cursor.current!.offsetTop
+        
+        console.log({offsetLeft,offsetTop})
+
     }
 
-    function handleMouseOut() {
-        cursor.current!.style.translate = "0"
+    function handleMouseOut(e:React.MouseEvent<HTMLUListElement, MouseEvent>) {
+        e.stopPropagation()
+        cursor.current!.style.transform = `translate(0)`
     }
 
     function handleMouseMove(e:React.MouseEvent<HTMLUListElement, MouseEvent>) {
         e.stopPropagation()
         const target = e.nativeEvent
         
+        const { clientX,clientY } = target 
+        const windowAdjust = window.innerHeight * .25
+        console.log({windowAdjust})
+        console.log(clientX,clientY)
+        cursor.current!.style.transform = `translate(${clientX - 60}px, ${clientY - windowAdjust - 60}px)`
     }
 
     return (
         <div className="carosel-container">
-            <ul className="carosel-stage" ref={container} onScroll={(e) => handleScroll(e)} onMouseOut={handleMouseOut} onMouseEnter={(e) => handleMouseEnter(e)} onMouseMove={(e) => handleMouseMove(e)}>
+            <ul className="carosel-stage" ref={container} onScroll={(e) => handleScroll(e)} onMouseOut={(e) => handleMouseOut(e)} onMouseEnter={(e) => handleMouseEnter(e)} onMouseMove={(e) => handleMouseMove(e)}>
             {Companies.map(company => {
                 return (
                     <div className="carosel-panel">
@@ -59,10 +61,10 @@ const Carosel = () => {
             <div className="progress-bar-container">
                 <span ref={bar} className="progress-bar-thumb" style={{"width":"60%"}}></span>
             </div>
-            {/* <div className="cursor-takeover-container"> */}
+            <div className="cursor-takeover-container" >
                 <div className="cursor-takeover-cursor" ref={cursor}>
                     <span>DRAG</span>
-                {/* </div> */}
+                </div>
             </div>
             
         </div>
@@ -70,3 +72,16 @@ const Carosel = () => {
 }
 
 export default Carosel
+
+/*
+// const { clientX,clientY } = e
+        // console.log(clientX,clientY)
+        // const { offsetX,offsetY } = e.nativeEvent
+        // console.log(offsetX,offsetY)
+        // const { offsetLeft,offsetTop } = cursor.current!
+        // console.log(offsetLeft,offsetTop)
+        // const test = clientX - offsetLeft
+        // const testTwo = clientY - offsetTop
+        // console.log(test,testTwo)
+        // cursor.current!.style.translate = `${test - 50}px ${testTwo}px`
+*/
