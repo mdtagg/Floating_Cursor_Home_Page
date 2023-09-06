9f66f95d62ab8279757de993d105f7eb200c6091
Mike Tagg <mdtagg91@gmail.com>
2023-09-05 17:37:12 -0700

    company data is now imported from utils file

--- src/Carosel/index.tsx
+++ src/Carosel/index.tsx
@@ -1,30 +1,32 @@
 import "./index.css"
-import { useState,useRef,useEffect } from "react"
+import { useRef } from "react"
+import { getCompanyData } from "./utils/companies"
 
 const Carosel = () => {
 
-    const companies = [
-        {
-            "title":"Google",
-            "content": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, neque esse dolorem quisquam tempora, tempore ipsum inventore unde eos sed illum numquam harum? Ut, dolore? In mollitia facere vitae sunt?"
-        },
-        {
-            "title":"KFC",
-            "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste doloribus cumque possimus? Maxime, qui, neque recusandae voluptatum ducimus necessitatibus ut sed maiores iure repellat porro sequi nobis modi? Vitae, quia."
-        },
-        {
-            "title": "Wilson",
-            "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid odit eaque voluptatibus iste? Eos dolores minima quaerat quisquam porro dolor, natus commodi provident praesentium tempora architecto accusantium optio? Aliquam, quasi!"
-        },
-        {
-            "title": "AT&T",
-            "content": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus laboriosam quae dolore corporis voluptatum quasi, at eveniet qui laudantium sed harum cumque odio rerum a reiciendis cum aliquam suscipit accusamus!"
-        },
-        {
-            "title": "Patagonia",
-            "content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. A reprehenderit quaerat corrupti fuga ullam voluptates, sapiente aliquid repellat ad accusantium doloribus odit. Voluptatibus quas natus perspiciatis perferendis pariatur itaque necessitatibus."
-        }
-    ]
+    const companies = getCompanyData()
+    // const companies = [
+    //     {
+    //         "title":"Google",
+    //         "content": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, neque esse dolorem quisquam tempora, tempore ipsum inventore unde eos sed illum numquam harum? Ut, dolore? In mollitia facere vitae sunt?"
+    //     },
+    //     {
+    //         "title":"KFC",
+    //         "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste doloribus cumque possimus? Maxime, qui, neque recusandae voluptatum ducimus necessitatibus ut sed maiores iure repellat porro sequi nobis modi? Vitae, quia."
+    //     },
+    //     {
+    //         "title": "Wilson",
+    //         "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid odit eaque voluptatibus iste? Eos dolores minima quaerat quisquam porro dolor, natus commodi provident praesentium tempora architecto accusantium optio? Aliquam, quasi!"
+    //     },
+    //     {
+    //         "title": "AT&T",
+    //         "content": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus laboriosam quae dolore corporis voluptatum quasi, at eveniet qui laudantium sed harum cumque odio rerum a reiciendis cum aliquam suscipit accusamus!"
+    //     },
+    //     {
+    //         "title": "Patagonia",
+    //         "content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. A reprehenderit quaerat corrupti fuga ullam voluptates, sapiente aliquid repellat ad accusantium doloribus odit. Voluptatibus quas natus perspiciatis perferendis pariatur itaque necessitatibus."
+    //     }
+    // ]
     
     const bar = useRef<HTMLUListElement | null>(null)
 
---
+++ src/Carosel/utils/companies.ts
@@ -0,0 +1,26 @@
+
+
+export const getCompanyData = () => {
+    return [
+        {
+            "title":"Google",
+            "content": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, neque esse dolorem quisquam tempora, tempore ipsum inventore unde eos sed illum numquam harum? Ut, dolore? In mollitia facere vitae sunt?"
+        },
+        {
+            "title":"KFC",
+            "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste doloribus cumque possimus? Maxime, qui, neque recusandae voluptatum ducimus necessitatibus ut sed maiores iure repellat porro sequi nobis modi? Vitae, quia."
+        },
+        {
+            "title": "Wilson",
+            "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid odit eaque voluptatibus iste? Eos dolores minima quaerat quisquam porro dolor, natus commodi provident praesentium tempora architecto accusantium optio? Aliquam, quasi!"
+        },
+        {
+            "title": "AT&T",
+            "content": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus laboriosam quae dolore corporis voluptatum quasi, at eveniet qui laudantium sed harum cumque odio rerum a reiciendis cum aliquam suscipit accusamus!"
+        },
+        {
+            "title": "Patagonia",
+            "content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. A reprehenderit quaerat corrupti fuga ullam voluptates, sapiente aliquid repellat ad accusantium doloribus odit. Voluptatibus quas natus perspiciatis perferendis pariatur itaque necessitatibus."
+        }
+    ]
+}
