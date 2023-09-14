b055efc490cf52eed4b8e86176530cdb116ae5c4
Mike Tagg <mdtagg91@gmail.com>
2023-09-14 07:46:55 -0700

    scroll event listener added to window for fixed cursor position
    a handle scroll function has been added to the window upon mouse enter
    of the carousel and removed on mouse leave. This functions purpose is to
    adjust the y coordinate of the curosr when the use scrolls vertically
    inside of the carousel

--- src/Carosel/index.tsx
+++ src/Carosel/index.tsx
@@ -1,13 +1,31 @@
 import "./index.css"
-import { useState,useRef } from "react"
+import { useState,useRef,useEffect,useCallback } from "react"
 import { getCompanyData } from "./utils/companies"
 import Cursor from "./Cursor"
+
+type TCoords = {
+    x:number,
+    y:number
+}
 
 const Carousel = () => {
 
     const Companies = getCompanyData()
     const [ isMouseDown, setIsMouseDown ] = useState(false)
     const [ isHover, setIsHover ] = useState(false)
+    const [ isCarouselHover, setIsCarouselHover ] = useState(false)
+
+    const [ cursorCoords,setCursorCoords ] = useState({
+        x:0,
+        y:0
+    })
+    
+    const cursorCoordsRef = useRef(cursorCoords)
+    const setCoordState = (coords:TCoords) => {
+        cursorCoordsRef.current = coords
+        setCursorCoords(coords)
+    } 
+
     const scrollBar = useRef<HTMLUListElement | null>(null)
     const cursorContainer = useRef<HTMLDivElement | null>(null)
     const cursorOuter = useRef<HTMLDivElement | null>(null)
@@ -16,6 +34,7 @@ const Carousel = () => {
 
 
     function handleScroll(e:React.UIEvent<HTMLUListElement, UIEvent>) {
+        
         const target = e.target as HTMLUListElement
         const maxScroll = target.scrollWidth - target.clientWidth // Amount of overflow scroll
         const scrollPosition = ((maxScroll - target.scrollLeft) / maxScroll) * 100 //the percentage from 100 of scroll space left
@@ -28,23 +47,43 @@ const Carousel = () => {
         cursorStyle.transform = `translate(0)`
         cursorStyle.transition = `transform 1s`
         cursorStyle.position = "absolute"
-      
     }
 
     function handleMouseMove(e:React.MouseEvent<HTMLDivElement, MouseEvent>) {      
-        // const test = cursorContainer.current!.offsetHeight 
-        let { clientX,clientY } = e
-        // const cursorOffset = cursorOuter.current!.offsetTop
-        clientX -= window.innerWidth - 160 //adjusting the horizontal position by half the cursor width
-        clientY -= cursorOuter.current!.offsetTop + 80 // calculates the next position of the cursor height adjusted for half the cursor width
+        const totalPageOffset = carouselContainer.current!.offsetTop + cursorOuter.current!.offsetTop
+       
+        let { clientX,clientY,pageY } = e
+        pageY -= totalPageOffset + 80
         
+        clientX -= window.innerWidth - 160 //adjusting the horizontal position by half the cursor width
+        // clientY -= cursorOuter.current!.offsetTop + 80 // calculates the next position of the cursor height adjusted for half the cursor width
+        
+        setCoordState({x:clientX,y:pageY})
+
         if(isMouseDown) {
             carouselStage.current!.scrollLeft = carouselStage.current!.scrollLeft - e.movementX
         }
 
-        cursorOuter.current!.style.transform = `translate(${clientX}px,${clientY}px)`
+        cursorOuter.current!.style.transform = `translate(${clientX}px,${pageY}px)`
         cursorOuter.current!.style.transition = isMouseDown ? "unset": "transform 0.1s"
     }
+
+    const handleWindowScroll = useCallback(() => {
+        const { x,y } = cursorCoordsRef.current!
+        console.log({x,y})
+        console.log(window.scrollY)
+        const adjustedY = y - 42
+        cursorOuter.current!.style.transform = `translate(${x}px,${adjustedY + window.scrollY}px)`
+        cursorOuter.current!.style.transition = "transform unset"
+    },[])
+
+    useEffect(() => {
+        if(isCarouselHover) {
+            window.addEventListener("scroll",handleWindowScroll)
+        }else {
+            window.removeEventListener("scroll",handleWindowScroll)
+        }
+    },[isCarouselHover])
 
     return (
         <div 
@@ -53,14 +92,19 @@ const Carousel = () => {
             onMouseDown={() => setIsMouseDown(true)}
             onMouseUp={() => setIsMouseDown(false)}
             onMouseEnter={(e) => {
-                cursorOuter.current!.style.position = "fixed"
+                cursorOuter.current!.style.position = "absolute"
+                setIsCarouselHover(true)
                 handleMouseMove(e)
             }}
+
             ref={carouselContainer}
         >
             <div 
                 id="carousel-test"
-                onMouseLeave={(e) => handleMouseLeave(e)} 
+                onMouseLeave={(e) => {
+                    handleMouseLeave(e)
+                    setIsCarouselHover(false)
+                }} 
             >
             <ul 
                 id="carousel-stage"
