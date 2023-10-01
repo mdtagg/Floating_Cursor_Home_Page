7a985bd1303893c1d005d75d82e73ecfe5177157
Mike Tagg <mdtagg91@gmail.com>
2023-10-01 15:38:55 -0700

    Bug fix, incorrect cursor position on entry before cursor reset
    if the user enters the container before the cursor is reset upon leaving
    the cursor would be misaligned. The tranform property is set to 0
    on leave so the coordinate positions on entry needed to account for
    the difference between where the cursor is on entry and the starting
    position. cursorAdjustment state was created for this fix

--- src/Experimental/CursorTakeover/index.tsx
+++ src/Experimental/CursorTakeover/index.tsx
@@ -27,6 +27,8 @@ const CursorTakeover = (props:TCursorWrapper) => {
     const [ isMouseDown, setIsMouseDown ] = useState(false)
     const [ isAnchorHover, setIsAnchorHover ] = useState(false)
     const [ isContainerHover, setIsContainerHover ] = useState(false)
+    const [ cursorAdjustment,setCursorAdjustment ] = useState({top:0,left:0})
+    console.log(cursorAdjustment)
     
 
     const [ cursorCoords, setCursorCoords ] = useState({
@@ -54,25 +56,43 @@ const CursorTakeover = (props:TCursorWrapper) => {
     function handleMouseMove(e:React.MouseEvent<HTMLElement, MouseEvent>) {
 
         if(!isContainerHover) return;
+        const { x, y, offset, transition } = cursorCoordsRef.current!
 
         const cursorPosition = {
-            x:cursorCoordsRef.current!.x + e.movementX,
-            y:cursorCoordsRef.current!.y + e.movementY,
-            offset:cursorCoordsRef.current!.offset,
-            transition:cursorCoordsRef.current!.transition
+            x: x + e.movementX,
+            y: y + e.movementY,
+            offset: offset,
+            transition: transition
         }
         setCursorCoords(cursorPosition)
         cursorCoordsRef.current! = cursorPosition
     }
 
     function getElementOffset() {
-        const offsetParent = cursorOuter.current!.offsetParent as HTMLDivElement
-        const containerOffsetTop = offsetParent!.getBoundingClientRect().top + window.scrollY
-        const cursorOffset = offsetParent!.offsetHeight / 2
-        const totalOffsetTop = containerOffsetTop + cursorOffset
 
-        return { totalOffsetTop }
+        const { top, left } = cursorOuter.current!.getBoundingClientRect()
+
+        let test = {topAdjust:0,leftAdjust:0}
+        if(cursorAdjustment.top == 0 || cursorAdjustment.left == 0) setCursorAdjustment({top:top,left:left});
+        else {
+            test.topAdjust = top - cursorAdjustment.top
+            test.leftAdjust = left - cursorAdjustment.left
+        }
+
+        const { offsetHeight, offsetWidth } = cursorOuter.current!
+        const cursorOffsetTop = (top + offsetHeight / 2 + window.scrollY) - test.topAdjust
+        const cursorOffsetLeft = left + offsetWidth / 2 + window.scrollX - test.leftAdjust
+
+        return { cursorOffsetTop,cursorOffsetLeft }
     }
+
+    /*
+    Bug: the transform property gets set to 0 on leave but
+    getBoundingClientRect takes the value from where the cursor
+    outer is. Since the outer is still transitioning back
+    on leave the cursor coords are off by the amount
+    left for the cursor to get back to starting position
+    */
 
     const handleWindowScroll = useCallback(() => {
         const { x, y, offset } = cursorCoordsRef.current!
@@ -88,16 +108,20 @@ const CursorTakeover = (props:TCursorWrapper) => {
     function handleEnter(e:React.MouseEvent<HTMLElement, MouseEvent>) {
 
         if(isContainerHover) return 
+        
         const target = e.target as HTMLElement
         target.nodeName === "A" ? setIsAnchorHover(true) : setIsAnchorHover(false)
 
         setIsContainerHover(true)
 
-        const cursorOffsets = getElementOffset() // removes the total offset from top of cursor position and adds page the pageY coordinate
+        const cursorOffsets = getElementOffset() // removes the total offset from top of cursor position and adds page to the pageY coordinate
    
         let { pageX, pageY } = e
-        pageY -= cursorOffsets.totalOffsetTop
-        pageX -= window.innerWidth - 160 //adjusting the horizontal position by the cursor width
+        pageY -= cursorOffsets.cursorOffsetTop
+        pageX -= cursorOffsets.cursorOffsetLeft
+        // console.log({pageX,pageY})
+
+        // pageX -= window.innerWidth - 160 //adjusting the horizontal position by the cursor width
       
         const cursorPosition = {
             x:pageX,
